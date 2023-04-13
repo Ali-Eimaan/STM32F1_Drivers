5ba7bda9b074ff721681160e9c42b5c7dda96b99
Ali-Eimaan <m.a.eimaan8@gmail.com>
2023-04-13 14:22:43 +0500

    Added ADC Driver

--- ADC_DRIVER/ADC.c
+++ ADC_DRIVER/ADC.c
@@ -1,51 +1,73 @@
 #include "ADC.h"
 
-void PA4_ADC_INIT(void)
+void ADC_INIT(ADC_TypeDef *ADCx, uint8_t *PinsofADC, uint8_t NumOfConversions, Channel_Priority Priorities)
 {
-    RCC->APB2ENR |= GPIOAEN;
-    RCC->APB2ENR |= ADC1EN;
+    if (ADCx == ADC1)
+        RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+    else if (ADCx == ADC2)
+        RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;
+    
+    for (int i = 0; i < NumOfConversions; i++)
+        SetADCpins(ADCx, PinsofADC[i]);
 
-    GPIOA->CRL &= ~(1U<<16); 
-    GPIOA->CRL &= ~(1U<<17); 
-    GPIOA->CRL &= ~(1U<<18);
-    GPIOA->CRL &= ~(1U<<19);  
-
-    ADC1->SQR3 = ADC1CH4;
-    ADC1->SQR1 = SEQLEN1; 
-    ADC1->CR2 |= CR2_ADON;
+    setSequence(ADCx, Priorities);
+    ADCx->SQR1 |= NumOfConversions<<20;
+    ADCx->CR2 |= ADC_CR2_ADON;
 }
 
-void PA4_ADC_INTERRUPT_INIT(void)
+void SetADCpins(ADC_TypeDef *ADCx, ADC_ChannelDef CHx)
 {
-    RCC->APB2ENR |= GPIOAEN;
-    RCC->APB2ENR |= ADC1EN;
+    if (CHx < Channel8)
+        SetPinMode(GPIOA, PORTA, CHx, InputMode, AnalogInput);
+    else
+        SetPinMode(GPIOB, PORTB, (CHx - 8), InputMode, AnalogInput);
+}
 
-    GPIOA->CRL &= ~(1U<<16); 
-    GPIOA->CRL &= ~(1U<<17); 
-    GPIOA->CRL &= ~(1U<<18);
-    GPIOA->CRL &= ~(1U<<19);  
+void setSequence(ADC_TypeDef *ADCx, Channel_Priority Priorities)
+{
+    ADCx->SQR3 = Priorities.Priority0 << 0;
+    ADCx->SQR3 = Priorities.Priority1 << 5;
+    ADCx->SQR3 = Priorities.Priority2 << 10;
+    ADCx->SQR3 = Priorities.Priority3 << 15;
+    ADCx->SQR3 = Priorities.Priority4 << 20;
+    ADCx->SQR3 = Priorities.Priority5 << 25;
+    ADCx->SQR2 = Priorities.Priority6 << 0;
+    ADCx->SQR2 = Priorities.Priority7 << 5;
+    ADCx->SQR2 = Priorities.Priority8 << 10;
+    ADCx->SQR2 = Priorities.Priority9 << 15;
+}
 
-    ADC1->SQR3 = ADC1CH4;
-    ADC1->SQR1 = SEQLEN1; 
-    ADC1->CR2 |= CR2_ADON;
-    ADC1->CR1 |= CR1_EOCIE;
+void ADC_INTERRUPT_INIT(ADC_TypeDef *ADCx, uint8_t *PinsofADC, uint8_t NumOfConversions, Channel_Priority Priorities)
+{
+    if (ADCx == ADC1)
+        RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+    else if (ADCx == ADC2)
+        RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;
+
+    for (int i = 0; i < NumOfConversions; i++)
+        SetADCpins(ADCx, PinsofADC[i]);
+
+    setSequence(ADCx, Priorities);
+    ADCx->SQR1 |= NumOfConversions<<20; 
+    ADCx->CR2 |= ADC_CR2_ADON;
+    ADCx->CR1 |= ADC_CR1_EOCIE;
 
     NVIC_EnableIRQ(ADC1_IRQn);
 }
 
-void Start_Conversion(void)
+void Start_Conversion(ADC_TypeDef *ADCx)
 {
-    ADC1->CR2 |= CR2_SWSTART;
+    ADCx->CR2 |= ADC_CR2_SWSTART;
 }
 
-void Start_Conversion_Continous(void)
+void Start_Conversion_Continous(ADC_TypeDef *ADCx)
 {
-    ADC1->CR2 |= CR2_CONT;
-    ADC1->CR2 |= CR2_SWSTART;
+    ADC1->CR2 |= ADC_CR2_CONT;
+    ADC1->CR2 |= ADC_CR2_SWSTART;
 }
 
-uint32_t ADC_Read(void)
+uint32_t ADC_Read(ADC_TypeDef *ADCx)
 {
-    while(!(ADC1->SR & SR_EOC));
-    return ADC1->DR;
+    while(!(ADCx->SR & ADC_SR_EOC));
+    return ADCx->DR;
 }
--- ADC_DRIVER/ADC.h
+++ ADC_DRIVER/ADC.h
@@ -3,21 +3,57 @@
 
 #include "stdint.h"
 #include "stm32f1xx.h"
+#include "GPIO.h"
 
-#define ADC1EN              (1U<<9)
-#define GPIOAEN             (1U<<2)
-#define ADC1CH4             (4U<<0)
-#define SEQLEN1             (0x00U)
-#define CR2_ADON            (1U<<0)
-#define CR2_SWSTART         (1U<<30)
-#define SR_EOC              (1U<<1)
-#define CR2_CONT            (1U<<1)
-#define CR1_EOCIE           (1U<<5)
+typedef enum
+{
+    Channel0,
+    Channel1,
+    Channel2,
+    Channel3,
+    Channel4,
+    Channel5,
+    Channel6,
+    Channel7,
+    Channel8,
+    Channel9,
+    Channel10,
+    Channel11,
+    Channel12,
+    Channel13,
+    Channel14,
+    Channel15,
+    ChannelTemp,
+    ChannelVrefint
+}ADC_ChannelDef;
 
-void PA4_ADC_INIT(void);
-void Start_Conversion(void);
-uint32_t ADC_Read(void);
-void Start_Conversion_Continous(void);
-void PA4_ADC_INTERRUPT_INIT(void);
+typedef struct
+{
+    ADC_ChannelDef Priority0;
+    ADC_ChannelDef Priority1;
+    ADC_ChannelDef Priority2;
+    ADC_ChannelDef Priority3;
+    ADC_ChannelDef Priority4;
+    ADC_ChannelDef Priority5;
+    ADC_ChannelDef Priority6;
+    ADC_ChannelDef Priority7;
+    ADC_ChannelDef Priority8;
+    ADC_ChannelDef Priority9;
+    ADC_ChannelDef Priority10;
+    ADC_ChannelDef Priority11;
+    ADC_ChannelDef Priority12;
+    ADC_ChannelDef Priority13;
+    ADC_ChannelDef Priority14;
+    ADC_ChannelDef Priority15;
+    ADC_ChannelDef Priority16;
+}Channel_Priority;
+
+void SetADCpins(ADC_TypeDef *ADCx, ADC_ChannelDef CHx);
+void ADC_INIT(ADC_TypeDef *ADCx, uint8_t *PinsofADC, uint8_t NumOfConversions, Channel_Priority Priorities);
+void setSequence(ADC_TypeDef *ADCx, Channel_Priority Priorities);
+void Start_Conversion(ADC_TypeDef *ADCx);
+uint32_t ADC_Read(ADC_TypeDef *ADCx);
+void Start_Conversion_Continous(ADC_TypeDef *ADCx);
+void ADC_INTERRUPT_INIT(ADC_TypeDef *ADCx, uint8_t *PinsofADC, uint8_t NumOfConversions, Channel_Priority Priorities);
 
 #endif
